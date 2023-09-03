988fb4078f9d8dcd04a8e1ff302f94742175358e
RainerGrimm <rainer@grimm-jaud.de>
2023-09-03 18:16:49 +0200

    Added a new exercise.

---
+++ solutions/getEndianness.cpp
@@ -0,0 +1,13 @@
+#include <bit>
+#include <iostream>
+
+int main() {
+
+    if constexpr (std::endian::native == std::endian::big) {
+        std::cout << "big-endian" << '\n';
+    }
+    else if constexpr (std::endian::native == std::endian::little) {
+        std::cout << "little-endian"  << '\n';
+    }
+
+}

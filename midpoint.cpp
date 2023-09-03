a594ee1893164e361e742707ae69c016fa4f4a51
RainerGrimm <rainer@grimm-jaud.de>
2023-09-03 18:08:24 +0200

    Added new source files.

---
+++ midpoint.cpp
@@ -0,0 +1,24 @@
+// midpoint.cpp
+
+#include <limits>
+#include <numeric>
+#include <iostream>
+ 
+int main() {
+
+    std::cout << '\n';
+
+    int first = std::numeric_limits<int>::max(); 
+    int second = std::numeric_limits<int>::max() - 2;
+ 
+    std::cout << "first: " << first << '\n';
+    std::cout << "second: " << second << '\n';
+
+    std::cout << '\n';
+
+    std::cout << "(first + second) / 2: " << (first + second) / 2 << '\n';
+    std::cout << "std::midpoint(first, second): " << std::midpoint(first, second) << '\n';
+
+    std::cout << '\n';
+
+}
---
+++ sourceLocation.cpp
@@ -0,0 +1,19 @@
+// from cppreference.com
+
+#include <iostream>
+#include <string_view>
+#include <source_location>
+ 
+void log(std::string_view message,
+         const std::source_location& location = std::source_location::current())
+{
+    std::cout << "info:"
+              << location.file_name() << ':'
+              << location.line() << ' '
+              << message << '\n';
+}
+ 
+int main()
+{
+    log("Hello world!");
+}
